8fe5daca0810b3e9f79c81e29935499bec3d20bf
sandraigwegbu <99925559+sandraigwegbu@users.noreply.github.com>
2022-02-17 22:47:27 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+#This program generates a band name using the name of the city you grew up in and the name of your first pet.
+
+#Welcome message
+print("Welcome to the Band Name Generator.\n")
+
+#User inputs
+city = input("What's the name of the city you grew up in?\n")
+pet = input("What's the name of your first pet?\n")
+
+#Band name output
+print("\nYour band name could be " + city + " " + pet)
